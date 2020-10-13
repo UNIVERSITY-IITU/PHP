2f15a469866b59eb1931ae9d88c3f135b5add865
AggressiveGhost <42280068+AggressiveGhost@users.noreply.github.com>
2020-10-13 13:40:14 +0600

    g

--- project2/task5/login.php
+++ project2/task5/login.php
@@ -2,12 +2,9 @@
 //echo $_POST["login"];
 //echo $_POST["password"];
 
-
 if ($_POST["login"] == "user" && $_POST["password"] == "qwerty"){
-//    echo "hello";
     header("Location : profile.php");
-    exit();
 }
-//else{
-//    header("Location : h.html");
-//}
+else{
+    header("Location : h.html");
+}
